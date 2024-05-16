d681ab7c8903426b4827802ab7d32669491c73d7
Gasello <9yar9@mail.ru>
2024-05-16 19:00:20 +0200

    fix

--- src/ui/Filter/Filter.tsx
+++ src/ui/Filter/Filter.tsx
@@ -1,11 +1,11 @@
 "use client";
 import "./filter.scss";
-import LowerArrow from "../../../public/icons/lowerArrow.svg";
-import UpperArrow from "../../../public/icons/upperArrow.svg";
+// import LowerArrow from "../../../public/icons/lowerArrow.svg";
+// import UpperArrow from "../../../public/icons/upperArrow.svg";
 
 import block from "bem-cn";
 import { BaseComponentProps } from "@/src/types/base";
-import { Input } from "@/src/components/Input/Input";
+// import { Input } from "@/src/components/Input/Input";
 import { Button } from "@/src/components/Button/Button";
 import { ButtonType } from "@/src/components/Button/buttonType";
 import { fetchGenres } from "@/src/api/api";
@@ -37,19 +37,19 @@ export default function Filter() {
       <div className={b("wrapper")}>
         <h2 className={b("title")}>Ratings</h2>
         <div className={b("inputsBlock")}>
-          <Input
-            style={{ marginRight: 8 }}
-            placeholder="From"
-            pattern="/^[0-9]+$/"
-            startIcon={UpperArrow}
-            endIcon={LowerArrow}
-          />
-          <Input
-            placeholder="To"
-            pattern="/^[0-9]+$/"
-            startIcon={UpperArrow}
-            endIcon={LowerArrow}
-          />
+          {/*<Input*/}
+          {/*  style={{ marginRight: 8 }}*/}
+          {/*  placeholder="From"*/}
+          {/*  pattern="/^[0-9]+$/"*/}
+          {/*  startIcon={UpperArrow}*/}
+          {/*  endIcon={LowerArrow}*/}
+          {/*/>*/}
+          {/*<Input*/}
+          {/*  placeholder="To"*/}
+          {/*  pattern="/^[0-9]+$/"*/}
+          {/*  startIcon={UpperArrow}*/}
+          {/*  endIcon={LowerArrow}*/}
+          {/*/>*/}
         </div>
       </div>
       <Button style={{ marginTop: 30 }} type={ButtonType.Outline}>
